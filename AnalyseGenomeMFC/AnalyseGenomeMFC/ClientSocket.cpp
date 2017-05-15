e590010fb33b7dde39d0c7a58709b36bead98c6c
PereWoulpy <laurent.turpin@insa-lyon.fr>
2017-05-15 18:00:05 +0200

    OK

---
+++ AnalyseGenomeMFC/AnalyseGenomeMFC/ClientSocket.cpp
@@ -0,0 +1,20 @@
+// ClientSocket.cpp : fichier d'implémentation
+//
+
+#include "stdafx.h"
+#include "AnalyseGenomeMFC.h"
+#include "ClientSocket.h"
+
+
+// ClientSocket
+
+ClientSocket::ClientSocket()
+{
+}
+
+ClientSocket::~ClientSocket()
+{
+}
+
+
+// Fonctions membres ClientSocket
---
+++ AnalyseGenomeMFC/AnalyseGenomeMFC/ClientSocket.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Cible de la commande ClientSocket
+
+class ClientSocket : public CAsyncSocket
+{
+public:
+	ClientSocket();
+	virtual ~ClientSocket();
+};
+
+
